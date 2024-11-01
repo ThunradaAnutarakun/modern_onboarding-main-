7f4cc909337d99c5a31b1bf04c7fcc742b7baf0d
ThunradaAnutarakun <122350494+ThunradaAnutarakun@users.noreply.github.com>
2024-11-01 15:26:51 +0700

    Add files via upload

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+// import 'package:introduction_screen/introduction_screen.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
+
+// flutter pub add introduction_screen
+// $ flutter pub add shared_preferences
+
+// dependencies:
+// introduction_screen: ^3.1.11
+// shared_preferences: ^2.2.1
+
+// flutter pub get
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'App Screen',
+          style: TextStyle(
+              fontWeight: FontWeight.bold, color: Colors.orangeAccent),
+        ),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text(
+          'Hi user, Welcome to ABC Co. App',
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+            color: Colors.orangeAccent,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// Source:https://www.youtube.com/watch?v=uia0EbMU1xs&t=18s
